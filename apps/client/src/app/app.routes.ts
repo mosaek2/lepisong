6233fcdcdc82b4d0204d3f4b19fe68255e22d090
mosaek2 <mosaek2@gmail.com>
2025-08-09 10:58:38 +0900

    feat(client): add home component and set as default route

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: Home,
+    pathMatch: 'full',
+  },
+];
